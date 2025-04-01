08a12968ffa4862ee7fff62ab58e0aeb4beb4a4c
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-04-02 02:53:11 +0300

    extract main components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer class="footer">
+      <p>This site is designed to be used for training purposes at SoftUni.</p>
+    </footer>
+  );
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header class="header">
+      <div class="logo">
+        <span class="course">React Workshop - September 2023</span>
+      </div>
+    </header>
+  );
+}
